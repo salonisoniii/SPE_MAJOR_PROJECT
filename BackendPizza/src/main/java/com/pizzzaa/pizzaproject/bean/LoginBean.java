fd24c6a388fbd58394705ef4f07a7da6e23ea61b
salonisoniii <141387917+salonisoniii@users.noreply.github.com>
2024-05-20 16:51:48 +0530

    Delete BackendPizza directory

--- BackendPizza/src/main/java/com/pizzzaa/pizzaproject/bean/LoginBean.java
+++
@@ -1,9 +0,0 @@
-package com.pizzzaa.pizzaproject.bean;
-
-import lombok.Data;
-
-@Data
-public class LoginBean {
-    private String userName;
-    private String password;
-}
--- BackendPizza/src/main/java/com/pizzzaa/pizzaproject/repository/OrderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.pizzzaa.pizzaproject.repository;
-
-import com.pizzzaa.pizzaproject.model.PizzaOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<PizzaOrder, Long> {
-}
